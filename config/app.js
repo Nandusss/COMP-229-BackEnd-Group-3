e1ed79d4c4e5e4916cb813b1161e6615ee7536df
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-11-15 23:00:01 -0500

    Removed uncessessary modules, and front end stuffs

--- config/app.js
+++ config/app.js
@@ -17,12 +17,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let compress = require('compression');
 let bodyParser = require('body-parser');
 let session = require('express-session');
-let flash = require('connect-flash');
 let passport = require('passport');
 
 var indexRouter = require('../routes/index');
@@ -31,26 +29,11 @@ var inventoryRouter = require('../routes/adlist');
 
 var app = express();
 
-app.use(session({
-  saveUninitialized: true,
-  resave: true,
-  secret: "sessionSecret"
-}));
-
-
-// view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
 
 // Sets up passport
-app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
