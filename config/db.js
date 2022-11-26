a3c83cfe64f81f10f154e2c9195cb8edffd2808c
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-11-25 19:30:25 -0500

    reverted to main database url

--- config/db.js
+++ config/db.js
@@ -15,20 +15,20 @@
 */
 
 // In real project, never expose your credential in your code.
-let atlasDB = "mongodb+srv://Admin:Xe1zwQXxEToJslV9@cluster0.6esmdck.mongodb.net/CBS-testing?retryWrites=true&w=majority"
+let atlasDB = "mongodb+srv://Admin:Xe1zwQXxEToJslV9@cluster0.6esmdck.mongodb.net/CBS?retryWrites=true&w=majority"
 
 let mongoose = require('mongoose');
 
-module.exports = function(){
+module.exports = function () {
 
-    // Connect to the database
-    mongoose.connect(atlasDB);
+	// Connect to the database
+	mongoose.connect(atlasDB);
 
-    let mongodb = mongoose.connection;
-    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
-    mongodb.once('open', ()=>{
-        console.log('==== Connected to MongoDB ====');
-    });
+	let mongodb = mongoose.connection;
+	mongodb.on('error', console.error.bind(console, 'Connection Error:'));
+	mongodb.once('open', () => {
+		console.log('==== Connected to MongoDB ====');
+	});
 
-    return mongodb;
+	return mongodb;
 }
